2ee75b18a81b2bebb3e415561b741c04411efda8
Kiley Bobbitt <jkileybob@aol.com>
2018-11-16 00:38:46 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,12 @@
+require pry
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |styles_of_lang, value_hash|
     value_hash.each do |language, lang_attribute_hash|
       if !new_hash.has_key?(language)
         new_hash[language] = {
-          [:type][lang_attribute_hash][:type],
+          [:type]=>[lang_attribute_hash][:type],
+          binding.pry
           :style=>[]
         }
       end
